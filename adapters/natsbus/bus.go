dafc0092971aa5b8157f2934f43fb034d73387fe
Mart Walt <mawkeye@gmail.com>
2026-02-14 08:37:09 +0100

    Add natsbus NATS JetStream EventBus implementation
    Publish on Emit with durable consumer subscriptions. Events serialized
    as JSON with manual ACK/NAK for reliable delivery.

---
+++ adapters/natsbus/bus.go
@@ -0,0 +1,67 @@
+// Package natsbus provides a NATS JetStream implementation of the flowstate EventBus.
+// Uses Publish on Emit and durable consumers for subscriptions.
+package natsbus
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/nats-io/nats.go"
+	"github.com/mawkeye/flowstate/types"
+)
+
+// Bus implements flowstate.EventBus using NATS JetStream.
+type Bus struct {
+	js      nats.JetStreamContext
+	subject string
+}
+
+// New creates a new NATS JetStream EventBus.
+// The subject is the NATS subject to publish/subscribe on (e.g., "flowstate.events").
+func New(js nats.JetStreamContext, subject string) *Bus {
+	return &Bus{
+		js:      js,
+		subject: subject,
+	}
+}
+
+// Emit publishes an event to the NATS JetStream subject.
+func (b *Bus) Emit(_ context.Context, event types.DomainEvent) error {
+	data, err := json.Marshal(event)
+	if err != nil {
+		return fmt.Errorf("natsbus: marshal event: %w", err)
+	}
+
+	_, err = b.js.Publish(b.subject, data)
+	if err != nil {
+		return fmt.Errorf("natsbus: publish: %w", err)
+	}
+	return nil
+}
+
+// Handler processes events from the stream.
+type Handler func(ctx context.Context, event types.DomainEvent) error
+
+// Subscribe creates a durable consumer and processes events. Blocks until context is cancelled.
+func (b *Bus) Subscribe(ctx context.Context, durableName string, handler Handler) error {
+	sub, err := b.js.Subscribe(b.subject, func(msg *nats.Msg) {
+		var event types.DomainEvent
+		if err := json.Unmarshal(msg.Data, &event); err != nil {
+			_ = msg.Nak()
+			return
+		}
+		if err := handler(ctx, event); err != nil {
+			_ = msg.Nak()
+			return
+		}
+		_ = msg.Ack()
+	}, nats.Durable(durableName), nats.ManualAck())
+	if err != nil {
+		return fmt.Errorf("natsbus: subscribe: %w", err)
+	}
+	defer sub.Unsubscribe()
+
+	<-ctx.Done()
+	return ctx.Err()
+}
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue v1.20.32
 	github.com/aws/aws-sdk-go-v2/service/dynamodb v1.55.0
 	github.com/jackc/pgx/v5 v5.8.0
+	github.com/nats-io/nats.go v1.48.0
 	github.com/redis/go-redis/v9 v9.17.3
 )
 
@@ -22,6 +23,11 @@ require (
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
+	github.com/klauspost/compress v1.18.0 // indirect
+	github.com/nats-io/nkeys v0.4.11 // indirect
+	github.com/nats-io/nuid v1.0.1 // indirect
+	golang.org/x/crypto v0.37.0 // indirect
 	golang.org/x/sync v0.17.0 // indirect
+	golang.org/x/sys v0.32.0 // indirect
 	golang.org/x/text v0.29.0 // indirect
 )
